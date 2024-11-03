9bfaa0c7d6a3bb2192e5e39b8f21dbfa60cdc9b5
AlanDCE <escurraalandaniel@gmail.com>
2024-11-03 15:22:46 -0300

    Actualizar configuración de vue.config.js y otros ajustes

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Gestor-de-Tareas-con-VueJs-y-Bootstrap-Alan/'  // Nombre del repositorio
+    ? '/vuel-gestor-de-tarea-alan-escurra/'  // Asegúrate de que este sea el nombre exacto de tu repositorio
     : '/'
 });
